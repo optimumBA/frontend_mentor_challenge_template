941d43c831810f7545ec882584a69eb27622ba19
mbashia <mbashiavictor@gmail.com>
2024-08-20 05:45:08 +0300

    complete profile card component

--- challenges/profile_card_component/tailwind.config.js
+++ challenges/profile_card_component/tailwind.config.js
@@ -6,6 +6,25 @@ module.exports = {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
       },
+      fontFamily: {
+        kumbh: ['Kumbh Sans'],
+      },
+      fontWeight: {
+        extra_light: 200,
+        light: 500,
+        regular: 400,
+        bold: 700,
+        extra_bold: 800,
+      },
+      backgroundImage: {
+        'pattern-top-bottom':
+          "url('images/bg-pattern-top.svg'), url('images/bg-pattern-bottom.svg')",
+        'card-pattern': "url('images/bg-pattern-card.svg')",
+        'top-pattern': "url('images/bg-pattern-top.svg')",
+      },
+      backgroundPosition: {
+        'custom-top-bottom': 'right 48vw bottom 40vh, left 45vw top 44vh',
+      },
     },
   },
   plugins: [],
