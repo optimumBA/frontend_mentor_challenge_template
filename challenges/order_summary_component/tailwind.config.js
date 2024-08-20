b63862f71d7a9e2bf5b1b68cbf94622091ed5ea6
mbashia <mbashiavictor@gmail.com>
2024-08-20 09:31:50 +0300

    complete order summary component

--- challenges/order_summary_component/tailwind.config.js
+++ challenges/order_summary_component/tailwind.config.js
@@ -5,6 +5,27 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'pale-blue': 'hsl(225, 100%, 94%)',
+        'bright-blue': 'hsl(245, 75%, 52%)',
+        'very-pale-blue': 'hsl(225, 100%, 98%)',
+        'desaturated-blue': 'hsl(224, 23%, 55%)',
+        'dark-blue': 'hsl(223, 47%, 23%)',
+      },
+      fontFamily: {
+        red_hat: ['Red Hat Display'],
+      },
+      fontWeight: {
+        extra_light: 200,
+        regular: 500,
+        light: 400,
+        bold: 700,
+        extra_bold: 900,
+      },
+      backgroundImage: {
+        'mobile-pattern': "url('images/pattern-background-mobile.svg')",
+
+        'bottom-pattern': "url('images/pattern-background-desktop.svg')",
+        illustration: "url('images/illustration-hero.svg')",
       },
     },
   },
