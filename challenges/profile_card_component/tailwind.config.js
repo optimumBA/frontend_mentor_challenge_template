4a7ed7d2d55d55cf6618f8d91f8be47a56833085
mbashia <mbashiavictor@gmail.com>
2024-08-23 05:46:51 +0300

    fix profile component

--- challenges/profile_card_component/tailwind.config.js
+++ challenges/profile_card_component/tailwind.config.js
@@ -5,16 +5,19 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
+        'optimum-blue': '#009efc',
+        'optimum-darkblue': '#0389e1',
+        'dark-cyan': 'hsl(185, 75%, 39%)',
+        'dark-desaturated-blue': 'hsl(229, 23%, 23%)',
+        'dark-grayish-blue': 'hsl(227, 10%, 46%)',
+        'dark-gray': 'hsl(0, 0%, 59%)',
       },
       fontFamily: {
-        kumbh: ['Kumbh Sans'],
+        kumbh: ['Kumbh Sans', 'sans-serif'],
       },
       fontWeight: {
-        extra_light: 200,
-        light: 500,
         regular: 400,
         bold: 700,
-        extra_bold: 800,
       },
       backgroundImage: {
         'pattern-top-bottom':
@@ -23,7 +26,7 @@ module.exports = {
         'top-pattern': "url('images/bg-pattern-top.svg')",
       },
       backgroundPosition: {
-        'custom-top-bottom': 'right 48vw bottom 40vh, left 45vw top 44vh',
+        'custom-top-bottom': 'right 48vw bottom 40vh, left 53vw top 40vh',
       },
     },
   },
