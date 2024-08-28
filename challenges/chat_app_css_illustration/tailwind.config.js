5d561adcb352cfb1832f9c65e005b8c631f9a9aa
mbashia <mbashiavictor@gmail.com>
2024-08-28 06:22:10 +0300

    complete chat app css illustration

--- challenges/chat_app_css_illustration/tailwind.config.js
+++ challenges/chat_app_css_illustration/tailwind.config.js
@@ -6,6 +6,29 @@ module.exports = {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
       },
+      colors: {
+        'optimum-blue': '#009efc',
+        'optimum-darkblue': '#0389e1',
+        // Text
+        'pale-violet': 'hsl(276, 100%, 81%)',
+        'moderate-violet': 'hsl(276, 55%, 52%)',
+        'desaturated-dark-violet': 'hsl(271, 15%, 43%)',
+        'grayish-blue': 'hsl(206, 6%, 79%)',
+        'very-dark-desaturated-violet': 'hsl(271, 36%, 24%)',
+        'dark-grayish-violet': 'hsl(270, 7%, 64%)',
+        // Gradient
+        'light-magenta': 'hsl(293, 100%, 63%)',
+        'light-violet': 'hsl(264, 100%, 61%)',
+        // Secondary
+        white: 'hsl(0, 0%, 100%)',
+        'light-grayish-violet': 'hsl(270, 20%, 96%)',
+        'very-dark-desaturated-violet': 'hsl(271, 36%, 24%)',
+        'very-light-magenta': 'hsl(289, 100%, 72%)',
+        'left-text-bg': '#ede5f9',
+      },
+      fontFamily: {
+        rubik: ['Rubik', 'sans-serif'],
+      },
     },
   },
   plugins: [],
